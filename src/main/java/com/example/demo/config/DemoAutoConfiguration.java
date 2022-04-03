2c9e17697c54b393b90436965a2d571c8d039570
Bomou He <paakmau@outlook.com>
2022-04-03 21:37:13 +0800

    feat: add ConfigurationProperties

--- src/main/java/com/example/demo/config/DemoAutoConfiguration.java
+++ src/main/java/com/example/demo/config/DemoAutoConfiguration.java
@@ -2,13 +2,21 @@ package com.example.demo.config;
 
 import com.example.demo.service.FooService;
 import com.example.demo.service.FooServiceImpl;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@ConfigurationProperties(prefix = "demo")
 public class DemoAutoConfiguration {
+    private String bar = "bar";
+
+    public void setBar(String bar) {
+        this.bar = bar;
+    }
+
     @Bean
     public FooService fooService() {
-        return new FooServiceImpl();
+        return new FooServiceImpl(bar);
     }
 }
--- src/main/java/com/example/demo/service/FooServiceImpl.java
+++ src/main/java/com/example/demo/service/FooServiceImpl.java
@@ -1,8 +1,14 @@
 package com.example.demo.service;
 
 public class FooServiceImpl implements FooService {
+    final String bar;
+
+    public FooServiceImpl(String bar) {
+        this.bar = bar;
+    }
+
     @Override
     public String bar() {
-        return "bar";
+        return bar;
     }
 }
